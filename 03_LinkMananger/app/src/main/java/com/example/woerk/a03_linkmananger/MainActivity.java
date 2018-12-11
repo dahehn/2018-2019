f5181ef349a7c15aafaf4ff3983a3bad64b8db2f
hehnm <hehnm@edu.htl-villach.at>
2018-12-11 10:57:10 +0100

    no message

--- 03_LinkMananger/app/src/main/java/com/example/woerk/a03_linkmananger/MainActivity.java
+++ 03_LinkMananger/app/src/main/java/com/example/woerk/a03_linkmananger/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.woerk.a03_linkmananger;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // inflate the menu and add it to the existing (empty) menu
-        getMenuInflater().inflate(R.menu.manu_main,menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1:
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
